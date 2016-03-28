c4f63b75d2102f388ee0031c0022dd8116c738b5
wnorrisyork <william.norris-york@intel.com>
2016-03-28 14:28:13 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Look what I did!\n",
+}
 }
