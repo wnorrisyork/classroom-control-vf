bd7c46eaab8cc0b3d9313d131a6732108b612db4
wnorrisyork <william.norris-york@intel.com>
2016-03-30 13:21:47 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,16 @@
+define users::managed_user (
+$homedir = "/home/${title}",
+$group = $title,
+){
+## user account
+user {$title:
+  ensure => present,
+}
+## group account
+file {$homedir:
+  ensure => directory,
+  group => $group,
+  owner => $title,
+}
+## manage ${home}/.ssh directory
+}
