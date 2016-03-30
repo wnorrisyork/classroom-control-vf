89f997bac439a14b80eb0ad0cc37422d204db229
wnorrisyork <william.norris-york@intel.com>
2016-03-30 09:03:28 -0700

    Refactored to simplify and use variables

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,41 +1,30 @@
 class nginx {
+File {
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+}
+$modules = 'puppet:///modules/nginx/',
+
   package { 'nginx':
     ensure => present,
   }
-  file { '/var/www':
+  file { ['/var/www','/etc/nginx/conf.d']:
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/index.html',
+    source => "${modules}index.html",
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/nginx.conf',
+    source => "${modules}nginx.conf",
     require => Package['nginx'],
     notify => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
-  }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
+    source => "${modules}default.conf",
     require => Package['nginx'],
     notify => Service['nginx'],
   }
