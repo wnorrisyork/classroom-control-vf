83d3ac419ee9d436ed66e75feb1f53eb0b8db98f
wnorrisyork <william.norris-york@intel.com>
2016-03-31 10:13:56 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,12 @@
 class nginx (
-$root = undef,
-) {
-  case $::osfamily {
-    'redhat','debian' : {
-    $package = 'nginx'
-    $owner = 'root'
-    $group = 'root'
-    # $docroot = '/var/www'
-    $confdir = '/etc/nginx'
-    $serverblockdir = '/etc/nginx/conf.d'
-    $logdir = '/var/log/nginx'
-    # this will be used if we don't pass in a value
-    $default_docroot = '/var/www'
-  }
-  'windows' : {
-    $package = 'nginx-service'
-    $owner = 'Administrator'
-    $group = 'Administrators'
-    # $docroot = 'C:/ProgramData/nginx/html'
-    $confdir = 'C:/ProgramData/nginx'
-    $serverblockdir = 'C:/ProgramData/nginx/conf.d'
-    $logdir = 'C:/ProgramData/nginx/logs'
-    # this will be used if we don't pass in a value
-    $default_docroot = 'C:/ProgramData/nginx/html'
-  }
-  default : {
-    fail("Module ${module_name} is not supported on ${::osfamily}")
-  }
-}
-
-  # user the service will run as. Used in the nginx.conf.erb template
-  $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
-    'windows' => 'nobody',
-  }
-  # if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-  }
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+) inherits nginx::params {
   File {
     owner => $owner,
     group => $group,
