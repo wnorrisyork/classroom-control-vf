110c71fb79b615c41531338b4e4716ba56b861e0
wnorrisyork <william.norris-york@intel.com>
2016-03-31 14:24:00 -0700

    Update vhost.pp

--- site/nginx/manifests/vhost.pp
+++ site/nginx/manifests/vhost.pp
@@ -1,1 +1,29 @@
-
+define nginx::vhost (
+$port = '80',
+$servername = $title,
+$docroot = "${nginx::docroot}/vhosts/${title}",
+) {
+  File {
+    owner => $nginx::owner,
+    group => $nginx::group,
+    mode => '0664',
+  }
+  # Hack to work around lack of DNS. Not for production use!
+  host { $title:
+    ip => $::ipaddress,
+  }
+  file { "nginx-vhost-${title}":
+    ensure => file,
+    path => "${nginx::confdir}/conf.d/${title}.conf",
+    content => template('nginx/vhost.conf.erb'),
+    notify => Service['nginx'],
+  }
+  file { $docroot:
+    ensure => directory,
+    before => File["nginx-vhost-${title}"],
+  }
+  file { "${docroot}/index.html":
+    ensure => file,
+    content => template('nginx/index.html.erb'),
+  }
+}
